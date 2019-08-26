26847b7a0331879a0ecc50cc2b6409e47080af5e
Minong <minhk3259@gmail.com>
2019-08-27 03:55:20 +0900

    Updates

---
+++ precache-manifest.048f25609fa4ef126becf5b7038d187e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcae2bb9fcd0435643154077557562c5",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "5c36065485610beab0f1",
+    "url": "/demo/static/css/main.b686b570.chunk.css"
+  },
+  {
+    "revision": "139dd7dc982c21fbea6e",
+    "url": "/demo/static/js/2.1be3d6e0.chunk.js"
+  },
+  {
+    "revision": "5c36065485610beab0f1",
+    "url": "/demo/static/js/main.7b99f617.chunk.js"
+  },
+  {
+    "revision": "5c8874f79e61da612d40",
+    "url": "/demo/static/js/runtime~main.9b038017.js"
+  },
+  {
+    "revision": "09b8766157157f54a4afd27f6d2cb9be",
+    "url": "/demo/static/media/img1.09b87661.png"
+  },
+  {
+    "revision": "195d7d9883931d5d8c5da79f8d90d00f",
+    "url": "/demo/static/media/img2.195d7d98.jpg"
+  },
+  {
+    "revision": "c372074373cc008b5bff7cef0f061a40",
+    "url": "/demo/static/media/img3.c3720743.jpg"
+  }
+]);
